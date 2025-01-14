7e47021dffe59668a0e9c0f88b6e8e26f3988588
Shishir Pokhrel <pokhrel.sh@northeastern.edu>
2025-01-14 10:15:23 -0500

    add lannding page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,7 +1,11 @@
 function LandingPage (){
     return (
         <div>
-            <h1>Landing Page</h1>
+            <h1>How it works</h1>
+            <h2>Step 1: Search for your classes in this format: [CS 1111] (without the bracket)</h2>
+            <h2>Step 2: Add the classes you want to your cart</h2>
+            <h2>Step 3: Go to your cart and click the "Generate Schedule" button</h2>
+            <h2>Step 4: View your schedule and save it to your calendar</h2>
         </div>
     )
 }
